599dbcc284f343a7fc824c0bb19f9131d96b8a72
Bwest8 <brwest85@outlook.com>
2024-03-22 11:02:00 -0400

    sidebar updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,7 +96,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'docs',
             position: 'left',
             label: 'Manual',
           },
