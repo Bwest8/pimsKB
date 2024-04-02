34ba7d78ae61b2e10559c1c1889023161f7cd2f9
Bwest8 <brwest85@outlook.com>
2024-04-01 23:26:15 -0400

    Appendix A course table updates

--- src/components/CourseDataTable.js
+++ src/components/CourseDataTable.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import DataTable from 'react-data-table-component';
 import courseCodes from '../data/CourseCodes2023-24.json';
+import Link from '@docusaurus/Link';
 
 const CourseDataTable = () => {
   const [searchText, setSearchText] = useState('');
@@ -15,12 +16,16 @@ const CourseDataTable = () => {
       name: 'State Course Code',
       selector: row => row.stateCourseCode,
       sortable: true,
-      width: '150px',
+      width: '80px',
+      wrap: true,
+      wrapHeader: true,
+      
     },
     {
       name: 'State Course Name',
       selector: row => row.stateCourseName,
       sortable: true,
+      width: '320px',
     },
     {
       name: 'Subject Area',
@@ -33,7 +38,7 @@ const CourseDataTable = () => {
       cell: row => (
         <span>{row.isAP === "true" ? <span class="badge badge--primary">AP</span> : ''}</span>
       ),
-      width: '70px',
+      width: '64px',
     },
     {
       name: 'IB',
@@ -41,7 +46,7 @@ const CourseDataTable = () => {
       cell: row => (
         <span>{row.isIB === "true" ? <span class="badge badge--info">IB</span> : ''}</span>
       ),
-      width: '70px',
+      width: '64px',
     },
     {
       name: 'Dual Credit',
@@ -49,7 +54,7 @@ const CourseDataTable = () => {
       cell: row => (
         <span>{row.isDualCredit === "true" ? <span class="badge badge--success">DC</span> : ''}</span>
       ),
-        width: '70px',
+      width: '65px',
     },
     {
       name: 'CI',
@@ -76,9 +81,56 @@ const CourseDataTable = () => {
   );
 
   const ExpandableRowComponent = ({ data }) => (
-    <div>
-      <p><b>SCED Code:</b> {data.scedCode}</p>
-      <p><b>SCED Description:</b> <br />{data.scedDescription}</p>
+    <div style={{ padding: '1em', background: '#f9f9f9', borderRadius: '5px' }}>
+      <h2>{data.stateCourseName}</h2>
+      <table style={{ borderCollapse: 'collapse', width: '100%' }}>
+        <thead>
+          <tr>
+            <th style={{ border: '1px solid #ccc', padding: '0.5em', textAlign: 'left' }}>Course Template Field</th>
+            <th style={{ border: '1px solid #ccc', padding: '0.5em', textAlign: 'left' }}>Valid Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}>
+              29 - <Link to="../templates/course/#29---alternate-course-code">ALTERNATE COURSE CODE</Link>
+              </td>
+            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}><code>{data.stateCourseCode}</code></td>
+          </tr>
+          <tr>
+            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}>
+            32 - <Link to="../templates/course/#32---dual-credit">DUAL CREDIT</Link>
+            </td>
+            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}><code>{data.isDualCredit === 'true' ? 'Y' : 'N'}</code></td>
+          </tr>
+          <tr>
+            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}>
+              33 - <Link to="../templates/course/#33---advanced-placement-indicator">ADVANCED PLACEMENT INDICATOR</Link>
+            </td>
+            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}><code>{data.isAP === 'true' ? 'Y' : 'N'}</code></td>
+          </tr>
+          <tr>
+            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}>
+              40 - <Link to="../templates/course/#40---international-baccalaureate-indicator">INTERNATIONAL BACCALAUREATE INDICATOR</Link>
+            </td>
+            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}><code>{data.isIB === 'true' ? 'Y' : 'N'}</code></td>
+          </tr>
+          <tr>
+            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}>
+            79 - <Link to="../templates/course/#79---cambridge-advanced-course-indicator">CAMBRIDGE ADVANCED COURSE INDICATOR</Link>
+            </td>
+            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}><code>{data.isCI === 'true' ? 'Y' : 'N'}</code></td>
+          </tr>
+        </tbody>
+      </table>
+      <hr />
+      <p style={{ margin: '0 0 0.5em 0', fontWeight: 'bold', color: '#333' }}>
+        SCED Code: <span style={{ fontWeight: 'normal' }}>{data.scedCode}</span>
+      </p>
+      <p style={{ margin: '0 0 0.5em 0', fontWeight: 'bold', color: '#333' }}>
+        SCED Description: <br />
+        <span style={{ fontWeight: 'normal' }}>{data.scedDescription}</span>
+      </p>
     </div>
   );
 
@@ -126,7 +178,7 @@ const CourseDataTable = () => {
         />
       </div>
       <br />
-      <DataTable
+<DataTable
   columns={columns}
   data={filteredData}
   pagination
@@ -139,19 +191,21 @@ const CourseDataTable = () => {
   customStyles={{
     headRow: {
       style: {
-        minHeight: '56px',
+        minHeight: '75px', // You can adjust this value
       },
     },
     headCells: {
       style: {
         paddingLeft: '8px',
         paddingRight: '8px',
-        fontSize: '14px',
+        fontSize: '13px',
+        minHeight: '75px', // Add this line and adjust the value as needed
+        whiteSpace: 'pre-wrap', // Change this line
       },
     },
   }}
-  />
-    </div>
+/>   
+</div>
   );
 };
 
