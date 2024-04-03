26477b12c48bb13a4fbb6a7cd5962db3b5c5da2a
Bwest8 <brwest85@outlook.com>
2024-04-03 08:51:29 -0400

    course table updates

--- src/components/CourseDataTable.js
+++ src/components/CourseDataTable.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
-import DataTable from 'react-data-table-component';
-import courseCodes from '../data/CourseCodes2023-24.json';
-import Link from '@docusaurus/Link';
+//src/components/CourseDataTable.js
+
+import React, { useState } from "react";
+import DataTable from "react-data-table-component";
+import courseCodes from "../data/CourseCodes2023-24.json";
+import Link from "@docusaurus/Link";
 
 const CourseDataTable = () => {
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [expandedRows, setExpandedRows] = useState([]);
 
   const handleSearch = (event) => {
@@ -13,123 +15,206 @@ const CourseDataTable = () => {
 
   const columns = [
     {
-      name: 'State Course Code',
-      selector: row => row.stateCourseCode,
+      name: "State Course Code",
+      selector: (row) => row.stateCourseCode,
       sortable: true,
-      width: '80px',
+      width: "160px",
       wrap: true,
       wrapHeader: true,
-      
     },
     {
-      name: 'State Course Name',
-      selector: row => row.stateCourseName,
+      name: "State Course Name",
+      selector: (row) => row.stateCourseName,
       sortable: true,
-      width: '320px',
+      width: "320px",
     },
     {
-      name: 'Subject Area',
-      selector: row => row.subjectArea,
+      name: "Subject Area",
+      selector: (row) => row.subjectArea,
       sortable: true,
     },
     {
-      name: 'AP',
-      selector: row => row.isAP,
-      cell: row => (
-        <span>{row.isAP === "true" ? <span class="badge badge--primary">AP</span> : ''}</span>
+      name: "Attributes",
+      cell: (row) => (
+        <div>
+          {row.isAP === "true" && (
+            <span className="badge badge--primary">AP</span>
+          )}
+          {row.isIB === "true" && (
+            <span className="badge badge--info badge--black">IB</span>
+          )}
+          {row.isDualCredit === "true" && (
+            <span className="badge badge--success">DC</span>
+          )}
+          {row.isCI === "true" && (
+            <span className="badge badge--warning">CI</span>
+          )}
+          {row.isFutureReady === "true" && (
+            <span className="badge badge--secondary">FR</span>
+          )}
+        </div>
       ),
-      width: '64px',
-    },
-    {
-      name: 'IB',
-      selector: row => row.isIB,
-      cell: row => (
-        <span>{row.isIB === "true" ? <span class="badge badge--info">IB</span> : ''}</span>
-      ),
-      width: '64px',
-    },
-    {
-      name: 'Dual Credit',
-      selector: row => row.isDualCredit,
-      cell: row => (
-        <span>{row.isDualCredit === "true" ? <span class="badge badge--success">DC</span> : ''}</span>
-      ),
-      width: '65px',
-    },
-    {
-      name: 'CI',
-      selector: row => row.isCI,
-      cell: row => (
-        <span>{row.isCI === "true" ? <span class="badge badge--warning">CI</span> : ''}</span>
-      ),
-      width: '70px',
-    },
-    {
-      name: 'Future Ready',
-      selector: row => row.isFutureReady,
-      cell: row => (
-        <span>{row.isFutureReady === "true" ? <span class="badge badge--secondary">FR</span> : ''}</span>
-      ),
-      width: '70px',
+      width: "200px",
+      selector: (row) => row.badges,
+      headerClassName: "badges-header",
     },
   ];
 
-  const filteredData = courseCodes.filter((item) =>
-    Object.values(item).some((value) =>
+  const filteredData = courseCodes.filter((item) => {
+    const searchTerms = searchText.toLowerCase().split(" ");
+
+    // Check if any search term matches special terms
+    if (
+      searchTerms.some((term) => ["ap", "advanced", "placement"].includes(term))
+    ) {
+      return item.isAP === "true";
+    } else if (
+      searchTerms.some((term) =>
+        ["ib", "international", "baccalaureate"].includes(term)
+      )
+    ) {
+      return item.isIB === "true";
+    } else if (
+      searchTerms.some((term) => ["dc", "dual", "credit"].includes(term))
+    ) {
+      return item.isDualCredit === "true";
+    } else if (
+      searchTerms.some((term) => ["ci", "cambridge", "advanced"].includes(term))
+    ) {
+      return item.isCI === "true";
+    } else if (
+      searchTerms.some((term) => ["fr", "future", "ready"].includes(term))
+    ) {
+      return item.isFutureReady === "true";
+    } else if (searchTerms.some((term) => ["new", "course"].includes(term))) {
+      return item.IsNew === "true";
+    } else if (searchTerms.some((term) => ["editorial"].includes(term))) {
+      return item.IsEditorialUpdate === "true";
+    } else if (searchTerms.some((term) => ["substantive"].includes(term))) {
+      return item.IsSubstantiveUpdate === "true";
+    } else if (searchTerms.includes("update")) {
+      return (
+        item.IsEditorialUpdate === "true" || item.IsSubstantiveUpdate === "true"
+      );
+    }
+
+    // If no special terms are found, check all values
+    return Object.values(item).some((value) =>
       String(value).toLowerCase().includes(searchText.toLowerCase())
-    )
-  );
+    );
+  });
 
   const ExpandableRowComponent = ({ data }) => (
-    <div style={{ padding: '1em', background: '#f9f9f9', borderRadius: '5px' }}>
-      <h2>{data.stateCourseName}</h2>
-      <table style={{ borderCollapse: 'collapse', width: '100%' }}>
+    <div
+      style={{
+        padding: "1em",
+        background: "#f9f9f9",
+        fontSize: "0.8em",
+        border: "1px solid #ccc",
+        borderTop: "none",
+        borderRadius: "0 0 5px 5px",
+      }}
+    >
+      <h3>{data.stateCourseName}</h3>
+      <table style={{ borderCollapse: "collapse", width: "100%" }}>
         <thead>
           <tr>
-            <th style={{ border: '1px solid #ccc', padding: '0.5em', textAlign: 'left' }}>Course Template Field</th>
-            <th style={{ border: '1px solid #ccc', padding: '0.5em', textAlign: 'left' }}>Valid Value</th>
+            <th
+              style={{
+                border: "1px solid #ccc",
+                padding: "0.5em",
+                textAlign: "left",
+              }}
+            >
+              Course Template Field
+            </th>
+            <th
+              style={{
+                border: "1px solid #ccc",
+                padding: "0.5em",
+                textAlign: "left",
+              }}
+            >
+              Valid Value
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}>
-              29 - <Link to="../templates/course/#29---alternate-course-code">ALTERNATE COURSE CODE</Link>
-              </td>
-            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}><code>{data.stateCourseCode}</code></td>
+            <td style={{ border: "1px solid #ccc", padding: "0.5em" }}>
+              29 -{" "}
+              <Link to="../templates/course/#29---alternate-course-code">
+                ALTERNATE COURSE CODE
+              </Link>
+            </td>
+            <td style={{ border: "1px solid #ccc", padding: "0.5em" }}>
+              <b>
+                <code>{data.stateCourseCode}</code>
+              </b>
+            </td>
           </tr>
           <tr>
-            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}>
-            32 - <Link to="../templates/course/#32---dual-credit">DUAL CREDIT</Link>
+            <td style={{ border: "1px solid #ccc", padding: "0.5em" }}>
+              32 -{" "}
+              <Link to="../templates/course/#32---dual-credit">
+                DUAL CREDIT
+              </Link>
             </td>
-            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}><code>{data.isDualCredit === 'true' ? 'Y' : 'N'}</code></td>
+            <td style={{ border: "1px solid #ccc", padding: "0.5em" }}>
+              <b>
+                <code>{data.isDualCredit === "true" ? "Y" : "N"}</code>
+              </b>
+            </td>
           </tr>
           <tr>
-            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}>
-              33 - <Link to="../templates/course/#33---advanced-placement-indicator">ADVANCED PLACEMENT INDICATOR</Link>
+            <td style={{ border: "1px solid #ccc", padding: "0.5em" }}>
+              33 -{" "}
+              <Link to="../templates/course/#33---advanced-placement-indicator">
+                ADVANCED PLACEMENT INDICATOR
+              </Link>
             </td>
-            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}><code>{data.isAP === 'true' ? 'Y' : 'N'}</code></td>
+            <td style={{ border: "1px solid #ccc", padding: "0.5em" }}>
+              <b>
+                <code>{data.isAP === "true" ? "Y" : "N"}</code>
+              </b>
+            </td>
           </tr>
           <tr>
-            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}>
-              40 - <Link to="../templates/course/#40---international-baccalaureate-indicator">INTERNATIONAL BACCALAUREATE INDICATOR</Link>
+            <td style={{ border: "1px solid #ccc", padding: "0.5em" }}>
+              40 -{" "}
+              <Link to="../templates/course/#40---international-baccalaureate-indicator">
+                INTERNATIONAL BACCALAUREATE INDICATOR
+              </Link>
             </td>
-            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}><code>{data.isIB === 'true' ? 'Y' : 'N'}</code></td>
+            <td style={{ border: "1px solid #ccc", padding: "0.5em" }}>
+              <b>
+                <code>{data.isIB === "true" ? "Y" : "N"}</code>
+              </b>
+            </td>
           </tr>
           <tr>
-            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}>
-            79 - <Link to="../templates/course/#79---cambridge-advanced-course-indicator">CAMBRIDGE ADVANCED COURSE INDICATOR</Link>
+            <td style={{ border: "1px solid #ccc", padding: "0.5em" }}>
+              79 -{" "}
+              <Link to="../templates/course/#79---cambridge-advanced-course-indicator">
+                CAMBRIDGE ADVANCED COURSE INDICATOR
+              </Link>
             </td>
-            <td style={{ border: '1px solid #ccc', padding: '0.5em' }}><code>{data.isCI === 'true' ? 'Y' : 'N'}</code></td>
+            <td style={{ border: "1px solid #ccc", padding: "0.5em" }}>
+              <b>
+                <code>{data.isCI === "true" ? "Y" : "N"}</code>
+              </b>
+            </td>
           </tr>
         </tbody>
       </table>
-      <hr />
-      <p style={{ margin: '0 0 0.5em 0', fontWeight: 'bold', color: '#333' }}>
-        SCED Code: <span style={{ fontWeight: 'normal' }}>{data.scedCode}</span>
+      <p style={{ margin: "0 0 0.5em 0", fontWeight: "bold", color: "#333" }}>
+        SCED Code: <span style={{ fontWeight: "normal" }}>{data.scedCode}</span>
       </p>
-      <p style={{ margin: '0 0 0.5em 0', fontWeight: 'bold', color: '#333' }}>
+      <p style={{ margin: "0 0 0.5em 0", fontWeight: "bold", color: "#333" }}>
         SCED Description: <br />
-        <span style={{ fontWeight: 'normal' }}>{data.scedDescription}</span>
+        <span style={{ fontWeight: "normal", wordWrap: "break-word" }}>
+          {data.scedDescription}
+        </span>
       </p>
     </div>
   );
@@ -138,9 +223,9 @@ const CourseDataTable = () => {
     const rowId = row.id;
     const currentExpandedRows = expandedRows;
     const isRowExpanded = currentExpandedRows.includes(rowId);
-  
+
     if (isRowExpanded) {
-      setExpandedRows(currentExpandedRows.filter(id => id !== rowId));
+      setExpandedRows(currentExpandedRows.filter((id) => id !== rowId));
     } else {
       setExpandedRows([...currentExpandedRows, rowId]);
     }
@@ -148,64 +233,135 @@ const CourseDataTable = () => {
 
   const conditionalRowStyles = [
     {
-      when: row => row.IsNew === "true",
+      when: (row) => row.IsNew === "true",
       style: {
-        backgroundColor: 'rgba(255, 255, 0, 0.2)',
+        backgroundColor: "rgba(255, 255, 0, 0.2)",
       },
     },
     {
-      when: row => row.IsEditorialUpdate === "true",
+      when: (row) => row.IsEditorialUpdate === "true",
       style: {
-        backgroundColor: 'rgba(0, 255, 0, 0.2)',
+        backgroundColor: "rgba(0, 255, 0, 0.2)",
       },
     },
     {
-      when: row => row.IsSubstantiveUpdate === "true",
+      when: (row) => row.IsSubstantiveUpdate === "true",
       style: {
-        backgroundColor: 'rgba(0, 0, 255, 0.2)',
+        backgroundColor: "rgba(0, 0, 255, 0.2)",
+      },
+    },
+    {
+      when: (row) => expandedRows.includes(row.stateCourseCode),
+      style: {
+        fontWeight: "bold",
       },
     },
   ];
 
+  const badgeDescriptions = [
+    { label: "AP", description: "Advanced Placement" },
+    { label: "IB", description: "International Baccalaureate" },
+    { label: "DC", description: "Dual Credit" },
+    { label: "CI", description: "Cambridge Advanced" },
+    { label: "FR", description: "Future Ready" },
+  ];
+
+  const rowStyleDescriptions = [
+    { label: "New Course", color: "rgba(255, 255, 0, 0.2)" },
+    { label: "Editorial Update", color: "rgba(0, 255, 0, 0.2)" },
+    { label: "Substantive Update", color: "rgba(0, 0, 255, 0.2)" },
+  ];
+
+  const getBadgeClassName = (label) => {
+    switch (label) {
+      case "AP":
+        return "badge--primary";
+      case "IB":
+        return "badge--info badge--black";
+      case "DC":
+        return "badge--success";
+      case "CI":
+        return "badge--warning";
+      case "FR":
+        return "badge--secondary";
+      default:
+        return "";
+    }
+  };
+
   return (
     <div>
-      <div>
-        <input
-          type="text"
-          placeholder="Search..."
-          value={searchText}
-          onChange={handleSearch}
-        />
+      <div style={{ marginTop: "10px", fontSize: "0.8em" }}>
+        <span style={{ fontWeight: "bold" }}>Attribute Key:</span>
+        <div style={{ display: "flex", flexWrap: "wrap", marginTop: "5px" }}>
+          {badgeDescriptions.map((badge) => (
+            <div
+              key={badge.label}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginRight: "10px",
+              }}
+            >
+              <span className={`badge ${getBadgeClassName(badge.label)}`}>
+                {badge.label}
+              </span>
+              <span style={{ marginLeft: "5px" }}>{badge.description}</span>
+            </div>
+          ))}
+        </div>
       </div>
-      <br />
-<DataTable
-  columns={columns}
-  data={filteredData}
-  pagination
-  expandableRows
-  expandableRowExpanded={row => expandedRows.includes(row.stateCourseCode)}
-  expandableRowsComponent={ExpandableRowComponent}
-  onRowExpandToggled={handleRowExpand}
-  conditionalRowStyles={conditionalRowStyles}
-  keyField="stateCourseCode"
-  customStyles={{
-    headRow: {
-      style: {
-        minHeight: '75px', // You can adjust this value
-      },
-    },
-    headCells: {
-      style: {
-        paddingLeft: '8px',
-        paddingRight: '8px',
-        fontSize: '13px',
-        minHeight: '75px', // Add this line and adjust the value as needed
-        whiteSpace: 'pre-wrap', // Change this line
-      },
-    },
-  }}
-/>   
-</div>
+      <div style={{ marginTop: "10px", fontSize: "0.8em" }}>
+        <span style={{ fontWeight: "bold" }}>Row Highlight Key:</span>
+        <div style={{ display: "flex", flexWrap: "wrap", marginTop: "5px" }}>
+          {rowStyleDescriptions.map((style) => (
+            <div
+              key={style.label}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginRight: "10px",
+              }}
+            >
+              <div
+                style={{
+                  width: "20px",
+                  height: "20px",
+                  backgroundColor: style.color,
+                  marginRight: "5px",
+                }}
+              ></div>
+              <span>{style.label}</span>
+            </div>
+          ))}
+        </div>
+        <br />
+        <div>
+          <input
+            type="text"
+            id="search"
+            placeholder="Search..."
+            style={{ fontSize: "20px", width: "300px" }}
+            value={searchText}
+            onChange={handleSearch}
+          />
+        </div>{" "}
+      </div>
+      <hr />
+      <DataTable
+        columns={columns}
+        data={filteredData}
+        pagination
+        expandableRows
+        expandableRowExpanded={(row) =>
+          expandedRows.includes(row.stateCourseCode)
+        }
+        expandableRowsComponent={ExpandableRowComponent}
+        onRowExpandToggled={handleRowExpand}
+        conditionalRowStyles={conditionalRowStyles}
+        keyField="stateCourseCode"
+      />
+    </div>
   );
 };
 
