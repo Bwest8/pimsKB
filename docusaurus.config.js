98625975fec6630dab937b1f762b4b12bfa4f7fd
Bwest8 <brwest85@outlook.com>
2024-04-12 22:46:49 -0400

    css updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,8 +95,8 @@ const config = {
       zoom: {
         selector: ".markdown :not(em) > img",
         background: {
-          light: "rgb(255, 255, 255)",
-          dark: "rgb(50, 50, 50)",
+          light: "#EEEEF0",
+          dark: "#313131",
         },
         config: {
           // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
